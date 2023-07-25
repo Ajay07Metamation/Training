e1d1d0c638f783601327ecde4499750833a2c2ec
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-07-25 15:47:16 +0530

    "Modified the file with Assignment A02 Code"

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,18 +1,17 @@
 ﻿int random = new Random ().Next (1, 101); //Variable to store random number generated
 Console.Write ("Guess the number between 1 to 100: ");
-for (int tries=0; ;tries++ ) {
-    int guess = ReadInt();
-    if (guess == random) { // check the guess
-        Console.WriteLine ($"Your guess is correct. You took {tries} attempts."); break;
-    } 
-    else if (guess > random)
-        Console.WriteLine ("Your guess is too high");
-    else
-        Console.WriteLine ("Your guess is too low");
+for (int tries = 0; ; tries++) {
+   int guess = ReadInt ();
+   if (guess == random) { // check the guess
+      Console.WriteLine ($"Your guess is correct. You took {tries} attempts."); break;
+   } else if (guess > random)
+      Console.WriteLine ("Your guess is too high");
+   else
+      Console.WriteLine ("Your guess is too low");
 }
 int ReadInt () { // To get valid input from user and convert to integer
-    for(; ; ) {
-        string value = Console.ReadLine ();
-        if(int.TryParse (value, out int result))return result;
-    }
+   for (; ; ) {
+      string value = Console.ReadLine ();
+      if (int.TryParse (value, out int result)) return result;
+   }
 }
