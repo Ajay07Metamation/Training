5cc73425300425d2ee0641f77f24fef95af5702e
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-07-26 13:36:43 +0530

    Changes done to Assignment A02

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,17 +1,20 @@
-﻿int random = new Random ().Next (1, 101); //Variable to store random number generated
-Console.Write ("Guess the number between 1 to 100: ");
-for (int tries = 0; ; tries++) {
-   int guess = ReadInt ();
-   if (guess == random) { // check the guess
-      Console.WriteLine ($"Your guess is correct. You took {tries} attempts."); break;
-   } else if (guess > random)
-      Console.WriteLine ("Your guess is too high");
-   else
-      Console.WriteLine ("Your guess is too low");
-}
-int ReadInt () { // To get valid input from user and convert to integer
-   for (; ; ) {
+﻿//This program is Number Guessing game!!
+var game = ConsoleKey.Y;
+while (game == ConsoleKey.Y) {
+   int random = new Random ().Next (1, 101); //Variable to store random number generated
+   Console.Write ("Guess the number between 1 to 100: ");
+   for (int tries = 1; ; tries++) {
       string value = Console.ReadLine ();
-      if (int.TryParse (value, out int result)) return result;
+      if (!int.TryParse (value, out int guess) ||  guess <1 || guess >100) { // Getting valid integer input from user
+         Console.WriteLine ("Enter valid integer within the range"); continue;
+      }
+      if (guess == random) {  // check the guess
+         Console.WriteLine ($"Your guess is correct. You took {tries} attempts."); break;
+      }
+      string result = (guess > random) ? "Your guess is too high" : "Your guess is too low";
+      Console.WriteLine (result);
    }
+   Console.Write ("Do you want to play again (Y)es / (N)o :"); // Asking user whether he wants to play again
+   game = Console.ReadKey ().Key;
+   Console.WriteLine ();
 }
