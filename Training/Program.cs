a057c5e0f276e45665a776aa5613d7befccbc561
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-07-26 17:11:08 +0530

    Changes done in Assignment A02

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,20 +1,22 @@
 ﻿//This program is Number Guessing game!!
 var game = ConsoleKey.Y;
 while (game == ConsoleKey.Y) {
-   int random = new Random ().Next (1, 101); //Variable to store random number generated
+   //Variable to store random number generated
+   int secretnum = new Random ().Next (1, 101);
    Console.Write ("Guess the number between 1 to 100: ");
    for (int tries = 1; ; tries++) {
       string value = Console.ReadLine ();
-      if (!int.TryParse (value, out int guess) ||  guess <1 || guess >100) { // Getting valid integer input from user
+      if (!int.TryParse (value, out int guess) || guess < 1 || guess > 100) {
+         // Getting valid integer input from user
          Console.WriteLine ("Enter valid integer within the range"); continue;
       }
-      if (guess == random) {  // check the guess
+      if (guess == secretnum) {  // check the guess
          Console.WriteLine ($"Your guess is correct. You took {tries} attempts."); break;
       }
-      string result = (guess > random) ? "Your guess is too high" : "Your guess is too low";
-      Console.WriteLine (result);
+      Console.WriteLine ($"Your guess is too {(guess > secretnum ? "high" : "low")}");
    }
-   Console.Write ("Do you want to play again (Y)es / (N)o :"); // Asking user whether he wants to play again
+   // Asking user whether he wants to play again
+   Console.Write ("Do you want to play again (Y)es / (N)o :");
    game = Console.ReadKey ().Key;
    Console.WriteLine ();
 }
