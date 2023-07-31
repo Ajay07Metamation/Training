14e91534b1d29cb1e2ba8ccda43a149994089fb1
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-07-31 14:22:21 +0530

    The C# static directive is added. Changes done!

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,13 +1,13 @@
 ﻿//This Program makes the computer guess what number we think.
-//Think of a number
-Console.ForegroundColor = ConsoleColor.Cyan;
+using static System.Console;
+ForegroundColor = ConsoleColor.Cyan;
 int lowLimit = 0,highLimit = 100,baseLimit = (lowLimit + highLimit) / 2;
-Console.WriteLine ("Heyy computer I'm thinking of a number between 1 to 100" + '\n' + "Try to guess it ");
-Console.ResetColor();
 //Setting lowlimit,highlimit and mid range
+WriteLine ("Heyy user think of a number between 1 to 100" + '\n' + "The computer will guess it ");
+ResetColor();
 for (int tries = 1; tries <= 7; tries++) {
-   Console.Write ($"Is the number greater than {baseLimit} (Y)es / (N)o ?  ");
-   switch (Console.ReadKey ().Key) {
+   Write ($"Is the number greater than {baseLimit} (Y)es / (N)o ?  ");
+   switch (ReadKey ().Key) {
       case ConsoleKey.Y:
          lowLimit = baseLimit;
          break;
@@ -15,15 +15,15 @@ for (int tries = 1; tries <= 7; tries++) {
          highLimit = baseLimit;
          break;
       default:
-         Console.WriteLine ("Enter valid number");
+         WriteLine ("Enter valid number");
          break;
    } 
    //Reducing range after each iteration
    baseLimit = (lowLimit + highLimit) / 2;
-   Console.WriteLine ();
+   WriteLine ();
 }
 //Printing the guess
-Console.WriteLine ();
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine ($"The secret number is  {highLimit} ");
-Console.ResetColor ();
+WriteLine ();
+ForegroundColor = ConsoleColor.Green;
+WriteLine ($"The secret number is  {highLimit} ");
+ResetColor ();
