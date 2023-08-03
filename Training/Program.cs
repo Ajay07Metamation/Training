66a55982b7e2272f1d8adf54f4b319275b7b4dae
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-03 10:08:00 +0530

    Completed Assignment A02.2 number guessing game from LSB to MSB

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,34 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿//This Program makes the computer guess what number we think.
+using static System.Console;
+//Setting lowlimit,highlimit and mid range
+WriteLine ("Heyy user think of a number between 1 to 100" + '\n' + "The computer will guess it ");
+ResetColor ();
+double N = 2, remainder = 0, power = 0, secret = 0;
+for (int tries = 1; tries <= 7; tries++) {
+   //Finding the bits from LSB TO MSB
+   //Computing the secret number 
+   Write ($"Is the remainder when divided by {N} is {remainder} (Y)es / (N)o ?  ");
+   switch (ReadKey ().Key) {
+      case ConsoleKey.Y:
+         if (tries == 1) remainder = 0;
+         secret = secret + (0 * (Math.Pow (2, power)));
+         power++;
+         N = Math.Pow (2, power + 1);
+         break;
+      case ConsoleKey.N:
+         secret = secret + (1 * (Math.Pow (2, power)));
+         remainder = remainder + (N / 2);
+         power++;
+         N = Math.Pow (2, power + 1);
+         break;
+      default:
+         WriteLine ("Enter valid number");
+         break;
+   }
+   WriteLine ();
 }
+//Printing the guess
+WriteLine ();
+ForegroundColor = ConsoleColor.Green;
+WriteLine ($"The secret number is {secret}");
+ResetColor ();
