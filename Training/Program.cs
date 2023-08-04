2d9d72cad0202d466efa16ac81225204614b9bfc
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-04 12:02:28 +0530

    Restructed the program using LINQ operators

--- Training/Program.cs
+++ Training/Program.cs
@@ -3,34 +3,24 @@ using static System.Console;
 //Importing the words.txt file  
 string[] words = File.ReadAllLines ("D:\\OneDrive - Trumpf Metamation Pvt Ltd\\Desktop\\ASSIGNMENTS\\words.txt");
 char[] letters = new[] { 'U', 'X', 'A', 'L', 'T', 'N', 'E' };
-int points = 0, total = 0;
-//Creating a sorted dictionary to store the points and words as key value pairs
-SortedDictionary<string, int> result = new SortedDictionary<string, int> ();
-foreach (string word in words)
-   //Applying all conditions to the words list
-   //Word should have more than 4 letters
-   //Word should contain the first letter of the letters array
-   //Word should contain only letters in letters array
-   if (word.Length >= 4 && word.Contains (letters[0]) && word.All (letters.Contains)) {
-      //Calculating points based on whether it is a pangram or not
-      //Adding the elememts to dictionary 
-      //Condition to check if it is an pangram
-      if (letters.All(word.Contains)){
-         points = word.Length + 7;
-         result.Add (word, points);
-      } else {
-         points = word.Length == 4 ? 1 : word.Length;
-         result.Add (word, points);
-      }
-      total = total + points;
-   }
-//Printing the dictionary in descending order by value
-foreach (var item in result.OrderByDescending (x => x.Value))
-   //Condition to check if it is an pangram
-   if (letters.All (item.Key.Contains)) {
-      ForegroundColor = ConsoleColor.Green;
-      WriteLine ($"{item.Value,4}.   {item.Key}");
-      ResetColor ();
-   } else Console.WriteLine ($"{item.Value,4}.   {item.Key}");
+int total = 0;
+// USing LINQ operators to filter and order the words 
+foreach(var data in words.Where(isValid)
+                         .Select(getscore)
+                         .OrderByDescending(a =>a.score)
+                         .ThenBy(a => a.word)) {
+   if (data.pangram) ForegroundColor = ConsoleColor.Green;
+   Console.WriteLine ($"{data.score,4} {data.word}");
+   ResetColor();  
+   total += data.score;
+}
 WriteLine ("-----");
 WriteLine ($"{total,4}    Total");
+//Method to chgeck if the word is valid
+bool isValid (string word) => word.Length >= 4 && word.Contains (letters[0]) && word.All (letters.Contains);
+//Method for checking pangram and get score
+(string word, int score, bool pangram) getscore (String word) {
+   bool pangram = letters.All (word.Contains);
+   int score = (word.Length > 4 ? word.Length : 1) + (pangram ? 7 : 0);
+   return (word,score, pangram);
+}
