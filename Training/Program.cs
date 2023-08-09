e530484c06b4c9db65aa00415286f86ca0ff0557
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-09 17:03:10 +0530

    Completed T06

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,8 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
-}
+﻿using static System.Console;
+Write ("Enter a string : ");
+var word = ReadLine ().ToLower ().Replace (" ", "");
+var orig = word.ToCharArray ();
+var rev = word.ToCharArray ();
+Array.Reverse (rev);
+if (Enumerable.SequenceEqual (orig, rev)) WriteLine ("It is a Pallindrome");
+else WriteLine ("It is not a pallindrome");
