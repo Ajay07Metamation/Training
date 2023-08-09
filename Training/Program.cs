d28fa936b4c206b5498bf1e9feeef5f82dd42ae8
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-09 16:48:21 +0530

    Completed T01

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,24 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using static System.Console;
+var key = ConsoleKey.Y;
+while (key == ConsoleKey.Y) {
+   Write ("Enter a Number : ");
+   if (int.TryParse (Console.ReadLine (), out int num)) {
+      Write ("Do you want to convert it into (B)inary or (H)exadecimal ?  ");
+      switch (Console.ReadKey ().Key) {
+         case ConsoleKey.B:
+            WriteLine ();
+            WriteLine ($"The number in Binary is : {Convert.ToString (num, 2)}");
+            break;
+         case ConsoleKey.H:
+            WriteLine ();
+            WriteLine ($"The number in Hexadecimal is : {num:X}");
+            break;
+         default:
+            WriteLine ("Enter a valid key");
+            break;
+      }
+      WriteLine ("Do you want to convert again");
+      key = ReadKey ().Key;
+      WriteLine ();
+   }
 }
