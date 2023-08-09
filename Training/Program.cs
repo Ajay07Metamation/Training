39c3eeae233ab0761779fd9a4d04d51178906974
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-09 17:01:38 +0530

    Completed T05

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,14 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using static System.Console;
+Write ("Enter a number : ");
+int.TryParse (Console.ReadLine (), out int num);
+int i;
+if (num == 1) Console.WriteLine ("1 is neither prime nor composite");
+else {
+   for (i = 2; i <= num / 2; i++) {
+      if (num % i == 0) {
+         Console.WriteLine ("It is not a Prime Number");
+         break;
+      }
+   }
+   if (i > (num / 2)) Console.WriteLine ("It is a Prime Number");
 }
