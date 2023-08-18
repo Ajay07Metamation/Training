0be62dfd4e55408db1e72d6afa3c355681007d85
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-18 13:03:31 +0530

    Completed T09

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,19 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿//Diamond Pattern
+Console.Write ("Enter the height of the pattern : ");
+int height = int.Parse (Console.ReadLine ());
+for (int i = 0; i <= height; i++) {
+   for (int blank = 0; blank < height - i; blank++)
+      Console.Write (" ");
+   for (int j = 1; j <= (2 * i) - 1; j++) {
+      Console.Write ("*");
+   }
+   Console.WriteLine ();
+}
+for (int i = height - 1; i > 0; i--) {
+   for (int blank = 0; blank < height - i; blank++)
+      Console.Write (" ");
+   for (int j = 1; j <= (2 * i) - 1; j++) {
+      Console.Write ("*");
+   }
+   Console.WriteLine ();
 }
