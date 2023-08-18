8c98b47e115129efb27633fdb5260d6b9425f32b
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-18 10:45:59 +0530

    Completed T08

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,9 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿// Tables
+Console.Write ("Till which tables do you want to print : ");
+int count = int.Parse (Console.ReadLine ());
+for (int i = 1; i <= count; i++) {
+   for (int j = 1; j <= 10; j++) {
+      Console.WriteLine ($"{i}*{j} = {i * j}");
+   }
+   Console.WriteLine ();
 }
