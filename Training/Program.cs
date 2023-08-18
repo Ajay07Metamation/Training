3336d650ce3add4c1d752ef21feffa1d90fe3b2c
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-18 14:24:22 +0530

    Completed T11

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,12 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿//Digital root
+Console.Write ("Enter a Number : ");
+int.TryParse (Console.ReadLine (), out int num);
+while ((num.ToString ().Length) != 1) {
+   var elem = num.ToString ().ToCharArray ();
+   num = 0;
+   foreach (char ch in elem) {
+      num += int.Parse (ch.ToString ());
+   }
+   Array.Clear (elem, 0, elem.Length);
 }
+Console.WriteLine ($"Digital root : {num}");
