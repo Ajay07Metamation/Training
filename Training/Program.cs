c1ed256c214515dbb85c2535333f876344de0796
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-18 15:31:12 +0530

    Completed T12

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,17 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿//Pascal's Triangle
+Console.Write ("Enter number of rows : ");
+int.TryParse (Console.ReadLine (), out int row);
+int val = 1;
+Console.WriteLine ("Pascal's triangle");
+for (int i = 0; i < row; i++) {
+   for (int blank = 1; blank <= row - i; blank++)
+      Console.Write (" ");
+   for (int j = 0; j <= i; j++) {
+      if (j == 0 || i == 0 || j == i)
+         val = 1;
+      else
+         val = val * (i - j + 1) / j;
+      Console.Write (val + " ");
+   }
+   Console.WriteLine ();
 }
