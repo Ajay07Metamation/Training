c4055ae49e048d3518e68486fac942f63d3813af
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-18 13:09:44 +0530

    Completed T10

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,11 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
-}
+﻿//Reverse num and check for pallindrome
+Console.Write ("Enter a Number : ");
+string num = Console.ReadLine ();
+Console.WriteLine ($"Original Number  : {num}");
+var ori = num.ToCharArray ();
+var rev = num.ToCharArray ();
+Array.Reverse (rev);
+Console.Write ("Reversed String : ");
+Console.WriteLine (rev);
+if (Enumerable.SequenceEqual (ori, rev)) Console.WriteLine ("It is a Pallindrome");
+else Console.WriteLine ("It is not a pallindrome");
