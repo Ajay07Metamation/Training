b9f9fa8f9711ec96f0cc94a0887199b0b1cc4903
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-22 14:50:55 +0530

    Completed A04

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,10 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿string words = File.ReadAllText ("D:\\OneDrive - Trumpf Metamation Pvt Ltd\\Desktop\\ASSIGNMENTS\\words.txt");
+Dictionary<char, int> freq = new ();
+for (char ch = 'A'; ch <= 'Z'; ch++) freq[ch] = 0;
+foreach (var ch in words) {
+   if (ch is >= 'A' and <= 'Z')
+      freq[ch] = freq[ch] + 1;
+}
+foreach (var item in freq.OrderByDescending (a => a.Value).Take (7)) {
+   Console.WriteLine (item);
 }
