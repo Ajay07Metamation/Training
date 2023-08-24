538d0fc22cf1c04706dfc13484bcdd1bf09bb5c2
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-24 17:34:21 +0530

    Completed T13

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,17 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿// Program for checking Valid Password 
+using static System.Console;
+Write ("Enter your Password : ");
+string pass = Console.ReadLine () ?? "";
+WriteLine (valid (pass));
+String valid (string s) {
+   String result = "";
+   String spl = "!@#$%^&*()-+";
+   if (s.Length >= 6) {
+      if (!s.Any (char.IsDigit)) result += "Password should contain atleast 1 numerical \n";
+      if (!s.Any (char.IsUpper)) result += "Password should contain atleast 1 Upper case \n";
+      if (!s.Any (char.IsLower)) result += "Password should contain atleast 1 Lower case \n";
+      if (!s.Any (spl.Contains)) result += "Password should contain atleast 1 special character \n";
+      if (result == "") result = "Your Password is Strong";
+   } else result = "Password should contain atleast 6 characters";
+   return result;
 }
