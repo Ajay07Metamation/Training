667b852769c6ebe9e49906c00196357cf24b63cf
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-25 12:47:52 +0530

    Completed T15

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,20 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿//Program to check Isogram
+using static System.Console;
+bool breakouter = false;
+Write ("Enter a String : ");
+String letter = ReadLine () ?? "";
+//Using for loop
+for (int ch = 0; ch < letter.Length - 1; ch++) {
+   for (int ch1 = ch + 1; ch1 < letter.Length; ch1++) {
+      if (letter[ch] == letter[ch1]) {
+         WriteLine ("It is not an Isogram");
+         breakouter = true;
+         break;
+      }
+   }
+   if (breakouter == true) break;
 }
+if (breakouter == false) WriteLine ("It is an Isogram");
+// Using LINQ operator
+if (letter.GroupBy (c => c).Any (g => g.Count () > 1)) WriteLine ("It is not an Isogram");
+else WriteLine ("It is an Isogram");
