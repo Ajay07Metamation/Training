002ef5e1998594ecb90a7e43506d707856cd0d77
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-25 12:49:22 +0530

    Completed T16

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,9 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
-}
+﻿// ABECEDARIAN WORD
+using static System.Console;
+WriteLine ("Enter word to convert into ABECEDARIAN WORD");
+string word = Console.ReadLine () ?? "";
+var wordcheck = word.ToLower ().ToArray ();
+Array.Sort (wordcheck);
+if (wordcheck.GroupBy (x => x).Any (c => c.Count () == wordcheck.Length))
+   WriteLine ("It is not an ABECEDARIAN WORD");
+else Console.WriteLine ($"ABECEDARIAN WORD : {String.Concat (wordcheck)}");
