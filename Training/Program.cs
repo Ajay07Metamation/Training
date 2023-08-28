88357fd2a7a96e4ada23e98efb0650af627f5634
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-28 14:00:13 +0530

    Changes done in T13

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,17 +1,18 @@
 ﻿// Program for checking Valid Password 
 using static System.Console;
 Write ("Enter your Password : ");
-string pass = Console.ReadLine () ?? "";
-WriteLine (valid (pass));
-String valid (string s) {
-   String result = "";
-   String spl = "!@#$%^&*()-+";
+string pass = ReadLine () ?? "";
+WriteLine (IsValid (pass));
+string IsValid (string s) {
+   string result = "";
+   string spl = "!@#$%^&*()-+";
    if (s.Length >= 6) {
-      if (!s.Any (char.IsDigit)) result += "Password should contain atleast 1 numerical \n";
-      if (!s.Any (char.IsUpper)) result += "Password should contain atleast 1 Upper case \n";
-      if (!s.Any (char.IsLower)) result += "Password should contain atleast 1 Lower case \n";
-      if (!s.Any (spl.Contains)) result += "Password should contain atleast 1 special character \n";
-      if (result == "") result = "Your Password is Strong";
+      var res = "Password should contain atleast 1 ";
+      if (!s.Any (char.IsDigit)) result += res + "Numerical \n";
+      if (!s.Any (char.IsUpper)) result += res + "Upper case \n";
+      if (!s.Any (char.IsLower)) result += res + "Lower case \n";
+      if (!s.Any (spl.Contains)) result += res + "Special character !@#$%^&*()-+ ";
    } else result = "Password should contain atleast 6 characters";
+   if (result == "") result = "Your Password is Strong";
    return result;
 }
