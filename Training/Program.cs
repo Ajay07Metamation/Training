fb4dc80665b8558d3d713cd9bae9aa707672aa15
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-28 14:54:12 +0530

    Changes done in T16

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,9 +1,12 @@
 ﻿// ABECEDARIAN WORD
 using static System.Console;
 WriteLine ("Enter word to convert into ABECEDARIAN WORD");
-string word = Console.ReadLine () ?? "";
-var wordcheck = word.ToLower ().ToArray ();
-Array.Sort (wordcheck);
-if (wordcheck.GroupBy (x => x).Any (c => c.Count () == wordcheck.Length))
-   WriteLine ("It is not an ABECEDARIAN WORD");
-else Console.WriteLine ($"ABECEDARIAN WORD : {String.Concat (wordcheck)}");
+string word = ReadLine () ?? "";
+WriteLine (Abecedarian (word));
+string Abecedarian (string s) {
+   var wordcheck = word.ToLower ().ToArray ();
+   Array.Sort (wordcheck);
+   if (wordcheck.GroupBy (x => x).Any (c => c.Count () == wordcheck.Length))
+      return "ABECEDARIAN WORD : ";
+   return "ABECEDARIAN WORD : " + string.Join ("", wordcheck);
+} 
