611a931906361152df57d48117202575bcc9335e
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-28 12:24:40 +0530

    Changes done in T10

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,12 +1,11 @@
 ﻿//Reverse num and check for pallindrome
 Console.Write ("Enter a Number : ");
-int.TryParse(Console.ReadLine (),out int num);
-int orig = num;
-int rev = 0;
-while (num != 0) {
-   rev = (rev * 10) + num % 10;
-   num = num / 10;
-}
-Console.WriteLine (rev);
-if (orig ==rev) Console.WriteLine ("It is a Pallindrome");
-else Console.WriteLine ("It is not a pallindrome");
+if (int.TryParse (Console.ReadLine (), out int num)) {
+   int orig = num;
+   int rev = 0;
+   while (num != 0) {
+      rev = (rev * 10) + num % 10;
+      num = num / 10;
+   }
+   Console.WriteLine ("It is " + (orig == rev ? "a Palindrome" : "not a Palindrome"));
+} else Console.WriteLine ("Enter valid Number");
