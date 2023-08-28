349ac3178f9be638d42b0972b39c895896748a85
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-28 12:15:51 +0530

    Changes done in T09

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,19 +1,19 @@
 ﻿//Diamond Pattern
-Console.Write ("Enter the height of the pattern : ");
-int height = int.Parse (Console.ReadLine ());
-for (int i = 0; i <= height; i++) {
-   for (int blank = 0; blank < height - i; blank++)
-      Console.Write (" ");
-   for (int j = 1; j <= (2 * i) - 1; j++) {
-      Console.Write ("*");
+using static System.Console;
+Write ("Enter the height of the pattern : ");
+if (int.TryParse (Console.ReadLine () ?? "", out int height)) {
+   for (int i = 0; i <= height; i++) {
+      for (int blank = 0; blank < height - i; blank++)
+         Write (" ");
+      for (int j = 1; j <= (2 * i) - 1; j++)
+         Write ("*");
+      WriteLine ();
    }
-   Console.WriteLine ();
-}
-for (int i = height - 1; i > 0; i--) {
-   for (int blank = 0; blank < height - i; blank++)
-      Console.Write (" ");
-   for (int j = 1; j <= (2 * i) - 1; j++) {
-      Console.Write ("*");
+   for (int i = height - 1; i > 0; i--) {
+      for (int blank = 0; blank < height - i; blank++)
+         Write (" ");
+      for (int j = 1; j <= (2 * i) - 1; j++)
+         Write ("*");
+      WriteLine ();
    }
-   Console.WriteLine ();
-}
+}else WriteLine("Enter valid Number");
