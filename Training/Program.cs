a23a2808f11901f6efca0471c8d88e41f6dd4fae
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-28 12:55:45 +0530

    Changes done in T12

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,17 +1,19 @@
 ﻿//Pascal's Triangle
-Console.Write ("Enter number of rows : ");
-int.TryParse (Console.ReadLine (), out int row);
-int val = 1;
-Console.WriteLine ("Pascal's triangle");
-for (int i = 0; i < row; i++) {
-   for (int blank = 1; blank <= row - i; blank++)
-      Console.Write (" ");
-   for (int j = 0; j <= i; j++) {
-      if (j == 0 || i == 0 || j == i)
-         val = 1;
-      else
-         val = val * (i - j + 1) / j;
-      Console.Write (val + " ");
+using static System.Console;
+Write ("Enter number of rows : ");
+if (int.TryParse (ReadLine (), out int row)) {
+   int val = 1;
+   WriteLine ("Pascal's triangle");
+   for (int i = 0; i < row; i++) {
+      for (int blank = 1; blank <= row - i; blank++)
+         Write (" ");
+      for (int j = 0; j <= i; j++) {
+         if (j == 0 || i == 0 || j == i)
+            val = 1;
+         else
+            val = val * (i - j + 1) / j;
+         Write (val + " ");
+      }
+      WriteLine ();
    }
-   Console.WriteLine ();
-}
+} else WriteLine ("Enter Valid Number");
