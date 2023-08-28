642a86a40e0bb2ec14bf6e5930001fdf7873f65a
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-28 11:59:42 +0530

    Changes done in T06

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,8 +1,8 @@
 ﻿using static System.Console;
 Write ("Enter a string : ");
-var word = ReadLine ().ToLower ().Replace (" ", "");
-var orig = word.ToCharArray ();
-var rev = word.ToCharArray ();
-Array.Reverse (rev);
-if (Enumerable.SequenceEqual (orig, rev)) WriteLine ("It is a Pallindrome");
-else WriteLine ("It is not a pallindrome");
+var word = (ReadLine () ?? "").ToLower ().Replace (" ", "") ;
+var revword = "";
+for(int i = word.Length-1;i>=0;i--)
+   revword += word[i];
+if (Enumerable.SequenceEqual (word, revword)) WriteLine ("It is a Palindrome");
+else WriteLine ("It is not a palindrome");
