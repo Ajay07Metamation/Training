b345986c5f7c685bdc4e659508c4026aa9e905c4
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-28 14:34:14 +0530

    Changes done in T15

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,20 +1,22 @@
 ﻿//Program to check Isogram
 using static System.Console;
-bool breakouter = false;
 Write ("Enter a String : ");
 String letter = ReadLine () ?? "";
-//Using for loop
-for (int ch = 0; ch < letter.Length - 1; ch++) {
-   for (int ch1 = ch + 1; ch1 < letter.Length; ch1++) {
-      if (letter[ch] == letter[ch1]) {
-         WriteLine ("It is not an Isogram");
-         breakouter = true;
-         break;
-      }
-   }
-   if (breakouter == true) break;
+WriteLine(IsIsogram(letter));
+string IsIsogram(string s) {
+   WriteLine("Method - 1");
+   if (s.Length>= 1) {
+      for (int ch = 0; ch < letter.Length - 1; ch++)
+         for (int ch1 = ch + 1; ch1 < letter.Length; ch1++)
+            if (letter[ch] == letter[ch1]) {
+               return "It is not an Isogram\n";
+            } 
+      return "It is an Isogram\n";
+   }return "Invalid Input\n";
 }
-if (breakouter == false) WriteLine ("It is an Isogram");
 // Using LINQ operator
-if (letter.GroupBy (c => c).Any (g => g.Count () > 1)) WriteLine ("It is not an Isogram");
-else WriteLine ("It is an Isogram");
+WriteLine("Method - 2");
+if (letter.Length >= 1) {
+   if (letter.GroupBy (c => c).Any (g => g.Count () > 1)) WriteLine ("It is not an Isogram");
+   else WriteLine ("It is an Isogram");
+} else WriteLine ("Invalid Input");
