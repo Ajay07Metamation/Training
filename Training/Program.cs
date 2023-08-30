e3f05baab0663a6ea14f5836417dca7b386d6658
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-30 17:37:09 +0530

    Changes done in T16

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,12 +1,20 @@
 ﻿// ABECEDARIAN WORD
 using static System.Console;
-WriteLine ("Enter word to convert into ABECEDARIAN WORD");
-string word = ReadLine () ?? "";
-WriteLine (Abecedarian (word));
-string Abecedarian (string s) {
-   var wordcheck = word.ToLower ().ToArray ();
-   Array.Sort (wordcheck);
-   if (wordcheck.GroupBy (x => x).Any (c => c.Count () == wordcheck.Length))
-      return "ABECEDARIAN WORD : ";
-   return "ABECEDARIAN WORD : " + string.Join ("", wordcheck);
-} 
+WriteLine ("Enter how many words do you want to give");
+if (int.TryParse (ReadLine (), out int index)) {
+   string[] words = new string[index];
+   for (int i = 0; i < index; i++) {
+      WriteLine ($"Enter Word {i + 1} : ");
+      words[i] = ReadLine () ?? "";
+      if (words[i].All(char.IsLetter));
+      else {
+         WriteLine ("Invalid Input");
+         i--;
+      }
+   }
+   var sortedword = words.Where (word => word == string.Join ("", word.OrderBy (c => c)));
+   if (sortedword.Count () > 0)
+      WriteLine ($"The Longest ABECEDARIAN Word is : {sortedword.OrderByDescending (word => word.Length)
+                                                                .First ()}");
+   else WriteLine ("No Abecedarian word in the array");
+} else WriteLine ("Enter Valid Input");
