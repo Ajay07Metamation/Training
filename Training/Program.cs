e42156e8a706dfce1c71dafecd30169e995528d4
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-31 15:48:43 +0530

    Changes and formatting done in T16

--- Training/Program.cs
+++ Training/Program.cs
@@ -2,6 +2,9 @@
 using static System.Console;
 WriteLine ("Enter how many words do you want to give");
 if (int.TryParse (ReadLine (), out int index)) {
+   GetAbecedarian(index);
+} else WriteLine ("Enter Valid Input");
+void GetAbecedarian (int index) {
    string[] words = new string[index];
    for (int i = 0; i < index; i++) {
       WriteLine ($"Enter Word {i + 1} : ");
@@ -16,4 +19,4 @@ if (int.TryParse (ReadLine (), out int index)) {
                                                 $"{sortedword.OrderByDescending (word => word.Length).First ()}"
                                                 : "No Abecedarian word in the array";
    WriteLine (result);
-} else WriteLine ("Enter Valid Input");
+}
