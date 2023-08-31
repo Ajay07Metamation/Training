d2d815dc7f74a9d732c407043a48583617e84ce2
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-31 17:45:33 +0530

    All changes and formatting done in T14

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Program to remove Adjacent duplicate character
 using static System.Console;
 Write ("Enter a String : ");
-String letter = ReadLine () ?? "";
+string letter = ReadLine () ?? "";
 for (int ch = 0; ch < letter.Length - 1; ch++) {
    for (int ch1 = ch + 1; ch1 < letter.Length;) {
       if (letter[ch] == letter[ch1]) {
