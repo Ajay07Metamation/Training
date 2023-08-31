ccc8618430dfc35d833035c5289f61022adb2084
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-31 16:48:35 +0530

    Changes and formatting done in  T04

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,31 +1,30 @@
 ﻿using static System.Console;
 var key = ConsoleKey.Y;
 while (key == ConsoleKey.Y) {
-   Write ("Enter the first number : ");
-   int.TryParse (ReadLine (), out int num1);
-   Write ("Enter the second number : ");
-   int.TryParse (ReadLine (), out int num2);
-   (num1,num2) = (Math.Abs(num1),Math.Abs(num2));
-   Write ("Do you want to convert it into (L)CM or (G)CD ?  ");
-   switch (ReadKey ().Key) {
-      case ConsoleKey.L:
-         WriteLine ();
-         WriteLine ("LCM : " + Getlcm (num1, num2));
-         break;
-      case ConsoleKey.G:
-         WriteLine ();
-         WriteLine ("GCD : " + Getgcd (num1, num2));
-         break;
-      default:
-         WriteLine ("Enter a valid key");
-         break;
+   WriteLine ("Enter Two Non zero numbers : ");
+   if (int.TryParse (ReadLine (), out int num1) && int.TryParse (ReadLine (), out int num2) && num1 != 0 && num2 != 0) {
+      (num1, num2) = (Math.Abs (num1), Math.Abs (num2));
+      Write ("Do you want to convert it into (L)CM or (G)CD ?  ");
+      switch (ReadKey ().Key) {
+         case ConsoleKey.L:
+            WriteLine ();
+            WriteLine ("LCM : " + Getlcm (num1, num2));
+            break;
+         case ConsoleKey.G:
+            WriteLine ();
+            WriteLine ("GCD : " + Getgcd (num1, num2));
+            break;
+         default:
+            WriteLine ("Enter a valid key");
+            break;
       }
-      WriteLine ("Do you want to convert again");
+      WriteLine ("Do you want to convert again (Y)es or (N)o ? ");
       key = ReadKey ().Key;
       WriteLine ();
 
+   } else WriteLine ("Enter Valid Input (NON ZERO NUMBERS)");
 }
-int Getlcm (int num1,int num2)=> ((num1 * num2) / Getgcd (num1, num2));
+int Getlcm (int num1, int num2) => ((num1 * num2) / Getgcd (num1, num2));
 int Getgcd (int num1, int num2) {
    int gcd = 0;
    if (num1 > num2) {
