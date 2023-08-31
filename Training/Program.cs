b7af28c7f8e02374f6129e92e91b816172b33c8f
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-31 17:37:57 +0530

    All changes and formatting done in T11

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,9 +1,10 @@
 ﻿//Digital root
 Console.Write ("Enter a Number : ");
 int.TryParse (Console.ReadLine (), out int num);
-while (num.ToString().Length!= 1) {
+while (num.ToString ().Length != 1) {
    var digroot = num;
    num = 0;
-   foreach (var ch in digroot.ToString())
-      num += int.Parse(ch.ToString());
-}Console.WriteLine ($"Digital root : {num}");
+   foreach (var ch in digroot.ToString ())
+      num += int.Parse (ch.ToString ());
+}
+Console.WriteLine ($"Digital root : {num}");
