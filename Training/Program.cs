74f4839dc31edfdb81dd9771cfe8b504766c430a
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-31 15:22:28 +0530

    Changes done in T06

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,8 +1,13 @@
-﻿using static System.Console;
+﻿//Program to check palindrome
+using static System.Console;
 Write ("Enter a string : ");
-var word = (ReadLine () ?? "").ToLower ().Replace (" ", "") ;
-var revword = "";
-for(int i = word.Length-1;i>=0;i--)
-   revword += word[i];
-if (Enumerable.SequenceEqual (word, revword)) WriteLine ("It is a Palindrome");
-else WriteLine ("It is not a palindrome");
+var word = (ReadLine () ?? "").ToLower ().Replace (" ", "");
+if (word.Length != 0 && word.All (char.IsLetter)) {
+   var result = "It is a Palindrome";
+   for (int i = 0; i <= word.Length / 2 - 1; i++)
+      if (word[i] != word[word.Length - 1 - i]) {
+         result = "It is not a Palindrome";
+         break;
+      }
+   WriteLine (result);
+} else WriteLine ("Enter valid input");
