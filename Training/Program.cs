eec4081a54f630bbc2968c97e2d3b6203d0945a7
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-08-31 15:29:33 +0530

    Changes and Formatting done in T16

--- Training/Program.cs
+++ Training/Program.cs
@@ -6,15 +6,14 @@ if (int.TryParse (ReadLine (), out int index)) {
    for (int i = 0; i < index; i++) {
       WriteLine ($"Enter Word {i + 1} : ");
       words[i] = ReadLine () ?? "";
-      if (words[i].All(char.IsLetter));
-      else {
+      if (!words[i].All (char.IsLetter)) {
          WriteLine ("Invalid Input");
          i--;
       }
    }
    var sortedword = words.Where (word => word == string.Join ("", word.OrderBy (c => c)));
    string result = sortedword.Count () > 0 ? "The Longest ABECEDARIAN Word is : " +
-                                             $"{sortedword.OrderByDescending (word => word.Length).First ()}" 
-                                             : "No Abecedarian word in the array";
-   WriteLine (result);  
+                                                $"{sortedword.OrderByDescending (word => word.Length).First ()}"
+                                                : "No Abecedarian word in the array";
+   WriteLine (result);
 } else WriteLine ("Enter Valid Input");
