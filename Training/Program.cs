786ef0c969cdc26e0fe0558c5747d58fd43d6be6
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-01 14:44:41 +0530

    Completed T17

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,22 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿//Program to get all possible permutations of a string
+using static System.Console;
+string answer = "";
+Write ("Enter the string : ");
+string word = ReadLine () ?? "";
+if (word.Length > 0 && word.All (char.IsLetter)) {
+   Write ("\nAll Possible Permutations are : \n");
+   GetPermute (word, answer);
+} else WriteLine ("Enter Valid Input");
+void GetPermute (string word, string answer) {
+   if (word.Length == 0) {
+      Write ($"{answer}\n");
+      return;
+   }
+   for (int i = 0; i < word.Length; i++) {
+      char ch = word[i];
+      string leftstr = word.Substring (0, i);
+      string rightstr = word.Substring (i + 1);
+      string reststr = leftstr + rightstr;
+      GetPermute (reststr, answer + ch);
+   }
 }
