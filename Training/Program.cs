99ec9dc43b8953e13e4cb7ee1e414eda9cdd3077
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-01 16:47:04 +0530

    Completed T22

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,17 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿//Program to Interchange 2 Indexes of a number
+using static System.Console;
+double num = new Random ().Next (10, 10000000);
+WriteLine ($"The Original Number is : {num}");
+GetInterchanged (num);
+void GetInterchanged (double num) {
+   int numLength = num.ToString ().Length;
+   WriteLine ($"Enter 2 Indexes to be Interchanged within 0 to {numLength - 1}");
+   if (!int.TryParse (ReadLine (), out int index1) || !(index1 >= 0 && index1 < numLength) ||
+       !int.TryParse (ReadLine (), out int index2) || !(index2 >= 0 && index2 < numLength))
+      WriteLine ($"Enter Valid Index between 0 and {numLength}");
+   else {
+      var numword = num.ToString ().ToArray ();
+      (numword[index1], numword[index2]) = (numword[index2], numword[index1]);
+      WriteLine ($"The Interchanged Number is : {string.Join ("", numword)}");
+   }
 }
