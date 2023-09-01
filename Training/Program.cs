3b69545263c2975628be026f825721b8c8cecb10
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-01 14:55:30 +0530

    Completed T18

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,15 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿//Program to check for Amstrong Number
+using static System.Console;
+Write ("Enter a number to check for Armstrong Number : ");
+if (int.TryParse (ReadLine (), out int num) && num > 0)
+   GetAmstrong (num);
+else WriteLine ("Enter Valid Number");
+void GetAmstrong (int n) {
+   int exp = n.ToString ().Length;
+   double res = 0;
+   while (n > 0) {
+      res += Math.Pow ((n % 10), exp);
+      n /= 10;
+   }
+   WriteLine ($"It is {((res == num) ? "" : "not ")}an Armstrong Number");
 }
