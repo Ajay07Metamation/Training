33365c23b9305e65ac1119af4a299fabf6d7b324
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-01 15:39:06 +0530

    Completed T19

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,10 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿//Program to Get Factorial of a Given Number
+using static System.Console;
+WriteLine ("Enter the number to calculate factorial : ");
+if (int.TryParse (ReadLine (), out int num) && num >= 0)
+   WriteLine ($"The Factorial of {num} is : " + GetFactorial (num));
+else WriteLine ("Enter Valid Number");
+double GetFactorial (int n) {
+   if (n == 0) return 1;
+   return n * GetFactorial (n - 1);
 }
