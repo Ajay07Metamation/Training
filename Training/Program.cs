75b32377308360427d53cf57000aa8f99ed10eb1
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-01 16:18:09 +0530

    Completed T21

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,13 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿//Program to Swap 2 Numbers
+using static System.Console;
+WriteLine ("Enter the 2 numbers");
+if (!int.TryParse (ReadLine (), out int num1) || !int.TryParse (ReadLine (), out int num2))
+   WriteLine ("Enter Valid Number");
+else {
+   WriteLine ($"Original Numbers : num1 = {num1} num2 = {num2}");
+   GetSwap (num1, num2);
+}
+void GetSwap (int a, int b) {
+   (a, b) = (b, a);
+   WriteLine ($"Swapped Numbers  : num1 = {a} num2 = {b}");
 }
