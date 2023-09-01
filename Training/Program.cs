b14a41f9a989d0019718ce6bb866f1cdd2a7ee97
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-01 15:25:46 +0530

    Completed T18.1

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,25 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿//Program to get Nth Armstrong Number
+using static System.Console;
+internal class Program {
+   private static void Main (string[] args) {
+      if (args.Length == 1 && int.TryParse (args[0], out int index) && index > 0 && index <= 25)
+         GetNthArmstrong (index);
+      else WriteLine ("Enter a Valid Index between 1 and 25 ");
+   }
+   static void GetNthArmstrong (int index) {
+      int count = 0, num = 0;
+      double res = 0;
+      while (count < index) {
+         num++;
+         res = 0;
+         int check = num;
+         int exp = num.ToString ().Length;
+         while (check > 0) {
+            res += Math.Pow (check % 10, exp);
+            check /= 10;
+         }
+         if (res == num) count++;
+      }
+      WriteLine ($"The {index}th Armstrong Number : {res}");
+   }
 }
