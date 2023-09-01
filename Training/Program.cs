285ab675e7ff4bc479af3e36069ae96cd533921f
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-01 08:42:19 +0530

    All changes and formatting done in T08

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,10 +1,12 @@
 ﻿// Tables
 using static System.Console;
 Write ("Till which tables do you want to print : ");
-if (int.TryParse (ReadLine () ?? "", out int count)) {
-   for (int i = 1; i <= count; i++) {
+if (int.TryParse (ReadLine () ?? "", out int count) && count > 0) {
+   int i = 1;
+   while (i <= count) {
       for (int j = 1; j <= 10; j++)
          WriteLine ($"{i} * {j,2} = {i * j,-1}");
+      i++;
       WriteLine ();
    }
 } else WriteLine ("Enter a valid Number");
