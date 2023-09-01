9b82e0e628f6f1456cadeb9fca34f8cb3ad08713
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-01 16:06:58 +0530

    Completed T20

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,18 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿//Program to get Integral and Fractional part of a decimal
+using static System.Console;
+Write ("Enter any number : ");
+if (double.TryParse (ReadLine (), out double num))
+   GetDigit (num);
+else WriteLine ("Enter Valid Number");
+void GetDigit (double num) {
+   string numword = num.ToString ();
+   var n = numword.IndexOf ('.');
+   Write ("Integral Part : ");
+   for (int i = 0; i < numword.Length; i++) {
+      if (i == n) {
+         Write ($"\nFractional part : ");
+         i++;
+      }
+      Write (numword[i] + " ");
+   }
 }
