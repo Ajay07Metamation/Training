9553716221441c9aa2db489b4fe1531fb1240a48
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-04 17:48:59 +0530

    Doing Changes

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,22 +1,18 @@
 ﻿//Program to get all possible permutations of a string
 using static System.Console;
 string answer = "";
+string[] permuteWord = {};
 Write ("Enter the string : ");
 string word = ReadLine () ?? "";
 if (word.Length > 0 && word.All (char.IsLetter)) {
    Write ("\nAll Possible Permutations are : \n");
-   GetPermute (word, answer);
+   PrintPermute (word, answer);
 } else WriteLine ("Enter Valid Input");
-void GetPermute (string word, string answer) {
+void PrintPermute (string word, string answer) {
    if (word.Length == 0) {
       Write ($"{answer}\n");
       return;
    }
-   for (int i = 0; i < word.Length; i++) {
-      char ch = word[i];
-      string leftstr = word.Substring (0, i);
-      string rightstr = word.Substring (i + 1);
-      string reststr = leftstr + rightstr;
-      GetPermute (reststr, answer + ch);
-   }
+   for (int i = 0; i < word.Length; i++) 
+      PrintPermute (word[..i] + word[(i+1)..], answer + word[i]);
 }
