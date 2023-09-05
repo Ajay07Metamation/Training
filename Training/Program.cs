6831551fa18c3c4c3a9f1b99588c58e0b6e16bd9
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-05 09:43:35 +0530

    All changes and Formatting done in T17

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,18 +1,24 @@
 ﻿//Program to get all possible permutations of a string
 using static System.Console;
+
 string answer = "";
-string[] permuteWord = {};
 Write ("Enter the string : ");
+int count = 0;
 string word = ReadLine () ?? "";
+List<string> permuteWords = new List<string> ();
 if (word.Length > 0 && word.All (char.IsLetter)) {
-   Write ("\nAll Possible Permutations are : \n");
    PrintPermute (word, answer);
+   Write ($"\nTotal Possible Permutations are {count} : \n");
+   foreach (var permute in permuteWords)
+      WriteLine (permute);
 } else WriteLine ("Enter Valid Input");
+
 void PrintPermute (string word, string answer) {
    if (word.Length == 0) {
-      Write ($"{answer}\n");
+      count++;
+      permuteWords.Add (answer);
       return;
    }
-   for (int i = 0; i < word.Length; i++) 
-      PrintPermute (word[..i] + word[(i+1)..], answer + word[i]);
+   for (int i = 0; i < word.Length; i++)
+      PrintPermute (word[..i] + word[(i + 1)..], answer + word[i]);
 }
