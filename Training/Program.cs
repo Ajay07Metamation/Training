242142691efdbf953e110f232d71d439fc13a815
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-05 16:21:50 +0530

    All changes done in T22

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,16 +1,18 @@
 ﻿//Program to Interchange 2 Indexes of a number
 using static System.Console;
+
 double num = new Random ().Next (10, 10000000);
 WriteLine ($"The Original Number is : {num}");
-GetInterchanged (num);
-void GetInterchanged (double num) {
-   int numLength = num.ToString ().Length;
+PrintInterchanged (num);
+
+void PrintInterchanged (double num) {
+   var numword = num.ToString ().ToArray ();
+   int numLength = numword.Length;
    WriteLine ($"Enter 2 Indexes to be Interchanged within 0 to {numLength - 1}");
    if (!int.TryParse (ReadLine (), out int index1) || !(index1 >= 0 && index1 < numLength) ||
        !int.TryParse (ReadLine (), out int index2) || !(index2 >= 0 && index2 < numLength))
-      WriteLine ($"Enter Valid Index between 0 and {numLength}");
+      WriteLine ($"Enter Valid Index between 0 and {numLength - 1}");
    else {
-      var numword = num.ToString ().ToArray ();
       (numword[index1], numword[index2]) = (numword[index2], numword[index1]);
       WriteLine ($"The Interchanged Number is : {string.Join ("", numword)}");
    }
