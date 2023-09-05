b8d8aa22ee8ecca68c745d7708453a0d066faaea
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-05 10:48:25 +0530

    All changes and formatting in T18

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,15 +1,18 @@
-﻿//Program to check for Amstrong Number
+﻿//Program to check for Armstrong Number
 using static System.Console;
 Write ("Enter a number to check for Armstrong Number : ");
 if (int.TryParse (ReadLine (), out int num) && num > 0)
-   GetAmstrong (num);
+   WriteLine ("It is " + (IsArmstrong (num) ? "" : "not ") + "a Armstrong Number");
 else WriteLine ("Enter Valid Number");
-void GetAmstrong (int n) {
+bool IsArmstrong (int n) {
    int exp = n.ToString ().Length;
-   double res = 0;
+   int res = 0;
    while (n > 0) {
-      res += Math.Pow ((n % 10), exp);
+      int power = 1;
+      for (int i = 0; i < exp; i++)
+         power *= n % 10;
+      res += power;
       n /= 10;
    }
-   WriteLine ($"It is {((res == num) ? "" : "not ")}an Armstrong Number");
+   return (res == num) ? true : false;
 }
