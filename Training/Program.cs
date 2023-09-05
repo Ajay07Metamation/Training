91fe217045c711536e876aef0ed34ffbb7b03c7d
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-05 10:55:31 +0530

    All changes and formatting in T18

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,9 +1,11 @@
 ﻿//Program to check for Armstrong Number
 using static System.Console;
+
 Write ("Enter a number to check for Armstrong Number : ");
 if (int.TryParse (ReadLine (), out int num) && num > 0)
    WriteLine ("It is " + (IsArmstrong (num) ? "" : "not ") + "a Armstrong Number");
 else WriteLine ("Enter Valid Number");
+
 bool IsArmstrong (int n) {
    int exp = n.ToString ().Length;
    int res = 0;
