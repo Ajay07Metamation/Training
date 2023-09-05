8127c04b7e4b60a21d4d126c327f5679299ab50a
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-05 12:14:51 +0530

    All changes and formatting done in T19

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,10 +1,13 @@
 ﻿//Program to Get Factorial of a Given Number
 using static System.Console;
+
 WriteLine ("Enter the number to calculate factorial : ");
 if (int.TryParse (ReadLine (), out int num) && num >= 0)
    WriteLine ($"The Factorial of {num} is : " + GetFactorial (num));
 else WriteLine ("Enter Valid Number");
+
 double GetFactorial (int n) {
    if (n == 0) return 1;
+   if (n <= 2) return n;
    return n * GetFactorial (n - 1);
 }
