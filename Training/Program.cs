d7be352d910f64fcb8ddb24b8ccd0623b9a1eec7
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-05 11:54:55 +0530

    All changes and formatting done in T18.1

--- Training/Program.cs
+++ Training/Program.cs
@@ -3,19 +3,23 @@ using static System.Console;
 internal class Program {
    private static void Main (string[] args) {
       if (args.Length == 1 && int.TryParse (args[0], out int index) && index > 0 && index <= 25)
-         GetNthArmstrong (index);
+         PrintNthArmstrong (index);
       else WriteLine ("Enter a Valid Index between 1 and 25 ");
    }
-   static void GetNthArmstrong (int index) {
+
+   static void PrintNthArmstrong (int index) {
       int count = 0, num = 0;
-      double res = 0;
+      int res = 0;
       while (count < index) {
          num++;
          res = 0;
          int check = num;
          int exp = num.ToString ().Length;
          while (check > 0) {
-            res += Math.Pow (check % 10, exp);
+            int power = 1;
+            for (int i = 0; i < exp; i++)
+               power *= check % 10;
+            res += power;
             check /= 10;
          }
          if (res == num) count++;
