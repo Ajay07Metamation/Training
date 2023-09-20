5ff7f0190dd3f44c778c77b14ada52b8c0b4abdf
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-20 17:34:18 +0530

    Completed T23

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,17 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using static System.Console;
+
+Write ("Enter the Vote String : ");
+string vote = ReadLine ().ToLower().Replace(" ","");
+if (vote != null && vote.Length > 0 && vote.All (char.IsLetter)) {
+   var winner = GetWinner (vote);
+   WriteLine ($"The winner is {winner.Letter} with {winner.Lettercount} votes");
+} else Write ("Enter Valid Input");
+
+(char Letter,int Lettercount) GetWinner (string vote) {
+   var result = vote.GroupBy (x => x)
+                    .Select (x => new { letter = x.First (), lettercount = x.Count () })
+                    .OrderByDescending (x => x.lettercount)
+                    .FirstOrDefault ();
+   return (result.letter, result.lettercount);
 }
+
