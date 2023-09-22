c1044fc58594c748a546f322cd30a1481c6a392b
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-22 17:41:19 +0530

    Completed T25

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,23 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using static System.Console;
+
+Write ("Enter the character array : ");
+var chArr = ReadLine ().ToCharArray ();
+Write ("Enter the Special Character : ");
+var splCh = char.ToLower (ReadKey ().KeyChar);
+Write ("\nEnter the order of sorting (D)escending or (O) for Optional Parameter (Ascending) : ");
+var sortOrder = char.ToLower (ReadKey ().KeyChar);
+
+if (chArr.Length > 0 && chArr.All (char.IsLetter) && char.IsLetter (splCh) && (sortOrder == 'd' || sortOrder == 'o')) {
+   WriteLine ("\nThe Final Array is : ");
+   var result = sortOrder == 'o' ? GetSortArray (chArr, splCh) : GetSortArray (chArr, splCh, sort: sortOrder);
+   foreach (var ch in result) { Write (ch + " "); }
+} else WriteLine ("\nEnter Valid Input");
+char[] GetSortArray (in char[] charr, char splch, char sort = 'a') {
+   var SortArr = charr.Where (c => c != splch);
+   SortArr = (sort == 'a') ? SortArr.Order () : SortArr.OrderDescending ();
+   return charr.Contains (splch) ? SortArr.Concat (charr.Where (c => c == splch)).ToArray () : SortArr.ToArray ();
 }
+
+
+
+
