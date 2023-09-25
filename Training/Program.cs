e59eaf9b7c54e472f4a3e128b61d08c908dc8c4c
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-25 10:16:01 +0530

    Condition changes done

--- Training/Program.cs
+++ Training/Program.cs
@@ -2,7 +2,7 @@
 using static System.Console;
 
 WriteLine ("Enter the number to calculate factorial : ");
-if (ulong.TryParse (ReadLine (), out ulong num) && 0 <= num)
+if (ulong.TryParse (ReadLine (), out ulong num))
    WriteLine ($"The Factorial of {num} is : " + GetFactorial (num));
 else WriteLine ("Enter Valid Number");
 
@@ -11,3 +11,4 @@ ulong GetFactorial (ulong n) {
    if (n <= 2) return n;
    return n * GetFactorial (n - 1);
 }
+
