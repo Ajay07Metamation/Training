fd50a7027cb6624f0a6af27a6706b93ef242d181
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-25 11:09:24 +0530

    Changes done in T24

--- Training/Program.cs
+++ Training/Program.cs
@@ -4,7 +4,7 @@
 //Numbers of wrappers remaining after exchanging wrappers with chocolate and money remaining
 // Sample Input : 20 3 5      Sample Output : 7 2 2 
 
-///Chocolate Wrappers
+//Chocolate Wrappers
 using static System.Console;
 
 ///<sample>Getting the Money, Price of Chocolate and Wrappers Exchange for chocolate from user</sample>
@@ -16,9 +16,7 @@ Write ("Enter Number of Wrappers to be Exchanged for a chocolate :");
 int.TryParse (ReadLine (), out int wrappers);
 if (money > 0 && price > 0 && wrappers > 1) {
    var result = GetChocolate (money, price, wrappers);
-   WriteLine ($"\nThe Number of chocolates is {result.Choc}\n" +
-                      $"The Remaining Money is {result.RemMoney}\n" +
-                      $"The Remaining wrappers is {result.RemWrappers}");
+   WriteLine ($"\nThe Number of chocolates is {result.Choc}\nThe Remaining Money is {result.RemMoney}\nThe Remaining wrappers is {result.RemWrappers}");
 } else
    WriteLine ("Enter Valid Input");
 
