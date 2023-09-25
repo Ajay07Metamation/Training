3add67415954cacc1471ea85e392268c27593023
Ajay Karthikeyan <ajaykarthikeyan.n@metamation.com>
2023-09-25 15:31:23 +0530

    Changes done in T23

--- Training/Program.cs
+++ Training/Program.cs
@@ -4,25 +4,41 @@
 //Sample Input : aaABBcd Sample Output : a or A
 using static System.Console;
 
-///<summary>VOTING CONTEST</summary>
-
-Write ("Enter the Vote String : ");
-string vote = ReadLine ().ToLower ().Replace (" ", "");
+Write ("Enter the Vote String without spaces: ");
+string vote = ReadLine ().ToLower ();
 if (vote != null && vote.Length > 0 && vote.All (char.IsLetter)) {
    var winner = GetWinner (vote);
-   WriteLine ($"The winner is {winner.Letter} with {winner.Lettercount} votes");
+   WriteLine ($"METHOD 1 - The winner is {winner.Letter} with {winner.LetterCount} votes");
+   var winner2 = GetWinner2 (vote);
+   WriteLine ($"METHOD 2 - The winner is {winner2.Letter} with {winner2.LetterCount} votes");
 } else Write ("Enter Valid Input");
 
-///<summary>GetWinner : Returns the winner having highest number of votes</summary>
-///<summary>If 2 contestant have same number of votes, First contestant who got vote is winner</summary>
+//Method - 1 (USING LINQ OPERATORS) 
+///<summary>GetWinner : Returns the winner having highest number of votes 
+///If 2 contestant have same number of votes, First contestant who got vote is winner</summary>
 ///<param name="vote">String of votes</param>
 ///<return>Returns a tuple (multiple return Values) containg the highest occuring character and the number of occurences</return>
 
-(char Letter, int Lettercount) GetWinner (string vote) {
-   var result = vote.GroupBy (x => x)
-                    .Select (x => new { letter = x.First (), lettercount = x.Count () })
-                    .OrderByDescending (x => x.lettercount)
-                    .FirstOrDefault ();
-   return (result.letter, result.lettercount);
+(char Letter, int LetterCount) GetWinner (string vote) => vote.GroupBy (x => x)
+                                                              .Select (x => (x.First (), x.Count ()))
+                                                              .OrderByDescending (x => x.Item2)
+                                                              .FirstOrDefault ();
+
+
+
+//Method - 2 (USING DICTIONARY
+///<summary>GetWinner2 : Returns the winner having highest number of votes 
+///If 2 contestant have same number of votes, First contestant who got vote is winner</summary>
+///<param name="vote">String of votes</param>
+///<return>Returns a tuple (multiple return Values) containg the highest occuring character and the number of occurences</return>
+
+(char Letter, int LetterCount) GetWinner2 (string vote) {
+   // Dictionary to store letters and their count
+   Dictionary<char, int> count = new ();
+   foreach (var ch in vote) 
+      count[ch] = !count.ContainsKey (ch) ? 1 : count[ch] + 1 ;
+   var result = count.OrderByDescending (x => x.Value).FirstOrDefault ();
+   return (result.Key, result.Value);
 }
+
 
