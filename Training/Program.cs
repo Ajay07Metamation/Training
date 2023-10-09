957009bbcf8891622a620868ab93c0105eaefe47
Ajay Karthikeyan N <ajaykarthikeyan.n@metamation.com>
2023-10-09 14:39:25 +0530

    T25 - Sort and swap special character

--- Training/Program.cs
+++ Training/Program.cs
@@ -18,17 +18,14 @@ if (chArr.Length > 0 && chArr.All (char.IsLetter) && char.IsLetter (splCh) && (s
    foreach (var ch in result) { Write (ch + " "); }
 } else WriteLine ("\nEnter Valid Input");
 
-///<summary>GetSortArray : Given a character array A, along with special character S and sort order O (default order is Ascending),
-/// print the sorted array by keeping the elements matching S to the last of the array.</summary>
-/// <param name="charr">It is the character array </param>
-/// <param name="splch">It is the spl character </param>
+/// <summary>GetSortArray : Given a character array A, along with special character S and sort order O (default order is Ascending)</summary>
+/// <summary>Print the sorted array by keeping the elements matching S to the last of the array</summary>
+/// <param name="charr">It is the character array</param>
+/// <param name="splch">It is the spl character</param>
 /// <param name="sort">It is the sorting order</param>
+
 char[] GetSortArray (in char[] charr, char splch, char sort = 'a') {
    var SortArr = charr.Where (c => c != splch);
    SortArr = (sort == 'a') ? SortArr.Order () : SortArr.OrderDescending ();
    return charr.Any (x => x == splch) ? SortArr.Concat (charr.Where (c => c == splch)).ToArray () : SortArr.ToArray ();
 }
-
-
-
-
