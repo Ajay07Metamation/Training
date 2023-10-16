fe513ad13bea788605e4993dfb48377206690d88
Ajay Karthikeyan N <ajaykarthikeyan.n@metamation.com>
2023-10-16 12:48:29 +0530

    doing Changes

--- Training/Program.cs
+++ Training/Program.cs
@@ -6,42 +6,45 @@ OutputEncoding = new UnicodeEncoding ();
 
 // Array containing White coins
 string[] white = new[] { "\u2656", "\u2658", "\u2657", "\u2654",
-                         "\u2655", "\u2657", "\u2658", "\u2656", "\u2659" };
+                         "\u2655", "\u2657", "\u2658", "\u2656"};
 // Array containing black coins
-string[] black = new[] { "\u265C", "\u265E", "\u265D", "\u265B", "\u265A",
-                         "\u265D", "\u265E", "\u265C", "\u265F" };
+string[] black = new[] { "\u265C", "\u265E", "\u265D", "\u265B",
+                         "\u265A", "\u265D", "\u265E", "\u265C"};
 WriteLine ("\u250F" + String.Concat (Enumerable.Repeat ("\u2501\u2501\u2501\u2501\u2501\u2533", 7)) +
                    "\u2501\u2501\u2501\u2501\u2501\u2513");
 DrawLines ('V');
-PrintCoins (white, 'M');
+PrintCoins (white);
 DrawLines ('H');
-PrintCoins (white, 'P');
+PrintPawns ('W');
 for (int i = 0; i < 5; i++) {
     DrawLines ('V');
     DrawLines ('H');
     DrawLines ('V');
 }
-PrintCoins (black, 'P');
+PrintPawns ('B');
 DrawLines ('H');
 DrawLines ('V');
-PrintCoins (black, 'M');
+PrintCoins (black);
 WriteLine ("\u2517" + String.Concat (Enumerable.Repeat ("\u2501\u2501\u2501\u2501\u2501\u253B", 7)) +
            "\u2501\u2501\u2501\u2501\u2501\u251B");
 
-// To print the coins</summary>
-// name="pieces" Indicates the colour of coins
-// name="type" Main coins (M) or Pawn (P)
-void PrintCoins (string[] pieces, char type) {
+// This prints the main coins on the chess board</summary>
+// "pieces" Indicates the colour of coins
+// "type" Main coins (M) or Pawn (P)
+void PrintCoins (string[] pieces) {
     Write ("\u2503");
-    if (type == 'M')
-        foreach (var piece in pieces)
-            Write ($"  {piece}  \u2503");
-    if (type == 'P')
-        Write (String.Concat (Enumerable.Repeat ($"  {pieces[8]}  \u2503", 8)));
+    foreach (var piece in pieces)
+        Write ($"  {piece}  \u2503");
+    WriteLine ();
+}
+
+void PrintPawns (char colour) {
+    Write ("\u2503");
+    Write (String.Concat (Enumerable.Repeat ($"  {(colour == 'W' ? "\u2659" : "\u265F")}  \u2503", 8)));
     WriteLine ();
 }
 
 // Print Horizontal and vertical lines for grid
-// name = line" Horizontal (H) or vetical (v) Grid
+// "line" Horizontal (H) or vetical (v) Grid
 void DrawLines (char line) => WriteLine (line == 'H' ? "\u2523" + String.Concat (Enumerable.Repeat ("\u2501\u2501\u2501\u2501\u2501\u252b", 8))
                                                      : String.Concat (Enumerable.Repeat ("\u2503     ", 9)));
