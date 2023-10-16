f34b7a8781faeca9cd389d7148c0fc0bbfb673de
Ajay Karthikeyan N <ajaykarthikeyan.n@metamation.com>
2023-10-16 10:58:28 +0530

    Changes done in T02

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,66 +1,79 @@
-﻿using static System.Console;
+﻿//Numbers to words or roman numbers
+
+using static System.Console;
 
 var key = ConsoleKey.Y;
 while (key == ConsoleKey.Y) {
-   Write ("Enter a Number : ");
-   if (int.TryParse (ReadLine (), out int num) && num >= 0) {
-      Write ("Do you want to convert it into (W)ords or (R)oman Numbers ?  ");
-      if (ReadKey ().Key == ConsoleKey.W) {
-         WriteLine ();
-         NumToWords (num);
-      } else if (ReadKey ().Key == ConsoleKey.R) {
-         WriteLine ();
-         NumToRoman (num);
-      } else
-         WriteLine ("Enter a valid key");
-      WriteLine ("\nDo you want to convert again (Y)es or (N)o ? ");
-      key = ReadKey ().Key;
-      WriteLine ();
-   } else WriteLine ("Enter Valid Number");
+    Write ("Enter a Number : ");
+    if (!int.TryParse (ReadLine (), out int num) && num >= 0) {
+        WriteLine ("Enter valid number");
+        continue;
+    }
+    Write ("Do you want to convert it into (W)ords or (R)oman Numbers ?  ");
+    var choice = ReadKey ().Key;
+    if (choice == ConsoleKey.W) {
+        WriteLine ();
+        NumToWords (num);
+    } else if (choice == ConsoleKey.R) {
+        WriteLine ();
+        NumToRomans (num);
+    } else WriteLine ("Enter a valid key");
+    WriteLine ("\nDo you want to convert again (Y)es or (N)o ? ");
+    key = ReadKey ().Key;
+    WriteLine ();
 }
 
+// Method to convert numbers to words using dictionary
 void NumToWords (int num) {
-   Dictionary<int, string> numtoword = new () { { 1, "One" }, { 2, "Two" }, { 3, "Three" }, { 4, "Four" }, { 5, "Five" },
-    { 6, "Six" }, { 7, "Seven" }, { 8, "Eight" }, { 9, "Nine" }, { 10, "Ten" },{11,"Eleven" },{12,"Twelve" },{13,"Thirteen"}
-   ,{14,"Fourteen" },{15,"Fifteen" },{16,"Sixteen" },{17,"Seventeen" },{18,"Eighteen" },{19,"Nineteen" },{20,"Twenty" },
-    {30,"Thirty" },{40,"Fourty" },{50,"Fifty" },{60,"Sixty"},{70,"Seventy" },{80,"Eighty" },{90,"Ninety" } };
-   Dictionary<int, string> endword = new () { { 3, " Hundred " }, { 4, " Thousand " }, { 5, " Thousand " }, { 6, " Lakhs " }, { 7, " Lakhs " }, { 8, " Crores " }, { 9, " Crores " } };
-   string result = "", end = "";
-   if (num == 0) WriteLine ("Zero");
-   while (num != 0) {
-      string numstr = num.ToString ();
-      int numlength = numstr.Length;
-      int divident = 0;
-      if (numlength == 1 || (numlength == 2 && num <= 20)) {
-         result += numtoword[num];
-         break;
-      } else if (numlength == 2 && num > 20)
-         divident = 1;
-      else
-         divident = numlength == 3 || numlength % 2 == 0 ? (int)Math.Pow (10, numlength - 1) : (int)Math.Pow (10, numlength - 2);
-      int quo = num / divident;
-      result += (quo.ToString ().Length == 2 && quo > 20 ? numtoword[quo - (quo % 10)] +
-                 " " + numtoword[quo % 10] : numtoword[quo]) + (numlength >= 3 ? endword[numlength] : "") + (numlength == 3 ? "and " : "");
-      num %= divident;
-   }
-   Write ($"{result}");
+    Dictionary<int, string> numtowords = new () {
+        [1] = "One", [2] = "Two", [3] = "Three", [4] = "Four",
+        [5] = "Five", [6] = "Six", [7] = "Seven", [8] = "Eight", [9] = "Nine", [10] = "Ten", [11] = "Eleven",
+        [12] = "Twelve", [13] = "Thirteen", [14] = "Fourteen", [15] = "Fifteen", [16] = "Sixteen", [17] = "Seventeen",
+        [18] = "Eighteen", [19] = "Nineteen", [20] = "Twenty", [30] = "Thirty", [40] = "Fourty", [50] = "Fifty",
+        [60] = "Sixty", [70] = "Seventy", [80] = "Eighty", [90] = "Ninety", [300] = " Hundred ",
+        [4000] = " Thousand ", [50000] = " Thousand ", [600000] = " Lakhs ", [7000000] = " Lakhs ",
+        [80000000] = " Crores ", [900000000] = " Crores "
+    };
+    string result = "";
+    if (num == 0) {
+        WriteLine ("Zero");
+        return;
+    }
+    while (num != 0) {
+        int numlength = num.ToString ().Length;
+        int divisor = 0;
+        if (numlength == 1 || (numlength == 2 && num <= 20)) {
+            result += numtowords[num];
+            break;
+        } else if (numlength == 2 && num > 20)
+            divisor = 1;
+        else
+            divisor = numlength == 3 || numlength % 2 == 0 ? (int)Math.Pow (10, numlength - 1) : (int)Math.Pow (10, numlength - 2);
+        int quo = num / divisor;
+        result += (quo.ToString ().Length == 2 && quo > 20 ? numtowords[quo - (quo % 10)] + " " + numtowords[quo % 10] : numtowords[quo])
+                + (numlength >= 3 ? numtowords[numlength * ((int)Math.Pow (10, numlength - 1))] : "")
+                + (numlength == 3 && num % 100 != 0 ? "and " : "");
+        num %= divisor;
+    }
+    Write ($"{result}");
 }
 
-void NumToRoman (int num) {
-   string[] roman = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
-   int[] number = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
-   string result = "";
-   if (num == 0) {
-      WriteLine ("No Roman Number representation for 0");
-      return;
-   }
-   while (num > 0) {
-      for (int i = number.Length - 1; i >= 0; i--)
-         if (num >= number[i]) {
-            result += roman[i];
-            num -= number[i];
-            i++;
-         }
-   }
-   Write (result);
+// Method to convert numbers to roman numbers using dictionary
+void NumToRomans (int num) {
+    string[] romans = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+    int[] numbers = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
+    string result = "";
+    if (num == 0) {
+        WriteLine ("No Roman Number representation for 0");
+        return;
+    }
+    while (num > 0) {
+        for (int i = numbers.Length - 1; i >= 0; i--)
+            if (num >= numbers[i]) {
+                result += romans[i];
+                num -= numbers[i];
+                i++;
+            }
+    }
+    Write (result);
 }
