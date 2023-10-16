877a2a74f504b22d99e3f52b90a8f71daf0ac17e
Ajay Karthikeyan N <ajaykarthikeyan.n@metamation.com>
2023-10-16 17:10:32 +0530

    All changes done in T28

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,6 @@
 ﻿//TQueue
-
 //Test Case
+using static System.Console;
 
 TQueue<int> queue = new TQueue<int> ();
 queue.Enqueue (1);
@@ -10,67 +10,68 @@ queue.Enqueue (4);
 queue.Enqueue (5);
 queue.Enqueue (6);
 queue.Display ();
-Console.WriteLine ("The Dequeued element is : " + queue.Dequeue ());
+WriteLine ("The Dequeued element is : " + queue.Dequeue ());
 queue.Display ();
-Console.WriteLine ("The Peek element is : " + queue.Peek ());
+WriteLine ("The Peek element is : " + queue.Peek ());
 
-/// <summary>Class TQueue implemented with array as underlying structure </summary>
+/// <summary>Represent a first in first out principle collection of objects</summary>
 
 class TQueue<T> {
-   //Private variable for initialising array and count
-   T[] arrayqueue;
-   int count;
+   // Private variable for initialising array and count
+   T[] mArrayQueue;
+   int mCount;
 
 
-   //Constructor for initializing the array and count
+   // Constructor for initializing the array and count
    public TQueue () {
-      arrayqueue = new T[4];
-      count = 0;
+      mArrayQueue = new T[4];
+      mCount = 0;
    }
 
-   //Property for getting count of elements in the queue
-   public int Count => count;
+   // Property for getting count of elements in the queue
+   public int Count => mCount;
 
-   //Property for getting Capacity of queue 
-   public int Capacity => arrayqueue.Length;
+   // Property for getting Capacity of queue 
+   public int Capacity => mArrayQueue.Length;
 
-   /// <summary>Method to add an element to the queue</summary>
-   /// <param name="a">element to be added</param>
+   /// <summary>Add/Enqueue an element to the queue</summary>
+   /// <param name="a">Element to be added</param>
    public void Enqueue (T a) {
-      if (count == Capacity) Array.Resize (ref arrayqueue, Capacity * 2);
-      arrayqueue[count] = a;
-      count++;
+      if (mCount == Capacity) Array.Resize (ref mArrayQueue, Capacity * 2);
+      mArrayQueue[mCount] = a;
+      mCount++;
    }
 
-   /// <summary>Method to delete the first inserted element </summary>
+   /// <summary>Delete/Dequeue the first inserted element</summary>
    /// <returns>The deleted element </returns>
    /// <exception cref="InvalidOperationException"></exception>
    public T Dequeue () {
       if (IsEmpty) throw new InvalidOperationException ();
-      var dequeuedElement = arrayqueue[0];
-      for (int i = 1; i < count; i++)
-         arrayqueue[i - 1] = arrayqueue[i];
-      count--;
+      var dequeuedElement = mArrayQueue[0];
+      for (int i = 1; i < mCount; i++)
+         mArrayQueue[i - 1] = mArrayQueue[i];
+      mCount--;
       return dequeuedElement;
    }
 
-   /// <summary>Method to find the peek element in the queue </summary>
+   /// <summary>Returns the peek element in the queue </summary>
    /// <returns>The first element of the queue</returns>
    /// <exception cref="InvalidOperationException"></exception>
    public T Peek () {
       if (IsEmpty) throw new InvalidOperationException ();
-      return arrayqueue[0];
+      return mArrayQueue[0];
    }
 
-   //Property to check if the queue is empty
-   public bool IsEmpty => count == 0;
+   // Property to check if the queue is empty
+   public bool IsEmpty => mCount == 0;
 
-   /// <summary>Method to print the elements of the queue </summary>
+   /// <summary>Print the elements of the queue</summary>
    public void Display () {
       if (IsEmpty) return;
-      Console.Write ("QUEUE : ");
-      for (int i = 0; i < count; i++) Console.Write (arrayqueue[i] + " ");
-      Console.WriteLine ();
+      Write ("QUEUE : ");
+      for (int i = 0; i < mCount; i++) Write (mArrayQueue[i] + " ");
+      WriteLine ();
    }
 
 }
+
