97142091d6c0ce96948ea3829c4a03c10919277a
Ajay Karthikeyan N <ajaykarthikeyan.n@metamation.com>
2023-11-01 17:11:31 +0530

    Changed enqueue and dequeue to O(1)

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,45 +1,86 @@
-﻿// TQueue
-// Implementation of queue using array
-// Test Case
+﻿// ----------------------------------------------------------------------------------------
+// Training 
+// Copyright (c) Metamation India.
+// ----------------------------------------------------------------------------------------
+// Program.cs
+// MyStack<T>
+// Create a stack (MyStack<T>) with underlying structure as array using property,methods and private variables
+// ----------------------------------------------------------------------------------------
 using static System.Console;
 
-TQueue<int> queue = new ();
-queue.Enqueue (1);
-queue.Enqueue (2);
-queue.Enqueue (3);
-queue.Enqueue (4);
-queue.Display ();
-WriteLine ("The Dequeued element is : " + queue.Dequeue ());
-queue.Display ();
-queue.Enqueue (5);
-queue.Enqueue (6);
-queue.Enqueue (7);
-queue.Enqueue (8);
-queue.Display ();
-WriteLine ("The Dequeued element is : " + queue.Dequeue ());
-queue.Display ();
-WriteLine ("The Peek element is : " + queue.Peek ());
+#region Program ----------------------------------------------------------------------
+internal class Program {
+   private static void Main (string[] args) {
+      TQueue<int> queue = new ();
+      queue.Enqueue (1);
+      queue.Enqueue (2);
+      queue.Enqueue (3);
+      queue.Enqueue (4);
+      queue.Display ();
+      WriteLine ("The dequeued element is : " + queue.Dequeue ());
+      WriteLine ("The dequeued element is : " + queue.Dequeue ());
+      queue.Display ();
+      queue.Enqueue (8);
+      WriteLine ("The peek element is : " + queue.Peek ());
+      queue.Display ();
+      queue.Enqueue (9);
+      queue.Enqueue (10);
+      queue.Enqueue (11);
+      queue.Enqueue (12);
+      queue.Enqueue (13);
+      queue.Enqueue (14);
+      queue.Enqueue (15);
+      queue.Display ();
+      WriteLine ("The peek element is : " + queue.Peek ());
+      WriteLine ("The dequeued element is : " + queue.Dequeue ());
+      WriteLine ("The dequeued element is : " + queue.Dequeue ());
+      WriteLine ("The dequeued element is : " + queue.Dequeue ());
+      WriteLine ("The dequeued element is : " + queue.Dequeue ());
+      WriteLine ("The dequeued element is : " + queue.Dequeue ());
+      WriteLine ("The dequeued element is : " + queue.Dequeue ());
+      queue.Enqueue (23);
+      WriteLine ("The peek element is : " + queue.Peek ());
+      queue.Display ();
+   }
+}
+#endregion
 
+#region Class MyStack ---------------------------------------------------------------------
 /// <summary>Represent a first in first out principle collection of objects</summary>
 class TQueue<T> {
 
+   #region Constructor -------------------------------------------
    // Constructor for initializing the array and count
    public TQueue () {
       mArrayQueue = new T[4];
       mCount = 0;
-   }
+      mFront = 0;
+      mRear = -1;
 
+   }
+   #endregion
+
+   #region Properties --------------------------------------------
    /// <summary>Returns the number of elements in the queue</summary>
    public int Count => mCount;
 
    /// <summary>Returns the capacity of the queue</summary>
    public int Capacity => mArrayQueue.Length;
 
+   /// <summary>Returns true or false after checking the Queue is empty or not</summary>
+   public bool IsEmpty => mCount == 0;
+   #endregion
+
+   #region Implementation ----------------------------------------
    /// <summary>Add/Enqueue an element to the queue</summary>
    /// <param name="a">Element to be added</param>
    public void Enqueue (T a) {
-      if (mCount == Capacity) Array.Resize (ref mArrayQueue, Capacity * 2);
-      mArrayQueue[mCount] = a;
+      if (mCount == Capacity) {
+         ArrayResize ();
+         Array.Resize (ref mArrayQueue, Capacity * 2);
+      }
+      mRear = (mRear + 1) % Capacity;
+      mArrayQueue[mRear] = a;
       mCount++;
    }
 
@@ -48,9 +89,12 @@ class TQueue<T> {
    /// <exception cref="InvalidOperationException"></exception>
    public T Dequeue () {
       if (IsEmpty) throw new InvalidOperationException ();
-      var dequeuedElement = mArrayQueue[0];
-      for (int i = 1; i < mCount; i++)
-         mArrayQueue[i - 1] = mArrayQueue[i];
+      var dequeuedElement = mArrayQueue[mFront];
+      mFront = (mFront + 1) % Capacity;
+      if (mFront == mRear) {
+         mRear = -1;
+         mFront = 0;
+      }
       mCount--;
       return dequeuedElement;
    }
@@ -60,22 +104,38 @@ class TQueue<T> {
    /// <exception cref="InvalidOperationException"></exception>
    public T Peek () {
       if (IsEmpty) throw new InvalidOperationException ();
-      return mArrayQueue[0];
+      return mArrayQueue[mFront];
    }
-
-   /// <summary>Returns true or false after checking the Queue is empty or not</summary>
-   public bool IsEmpty => mCount == 0;
 
    /// <summary>Print the elements of the queue</summary>
    public void Display () {
       if (IsEmpty) return;
       Write ("QUEUE : ");
-      for (int i = 0; i < mCount; i++) Write (mArrayQueue[i] + " ");
+      int i = mFront;
+      for (; i != mRear; i = (i + 1) % Capacity) Write (mArrayQueue[i] + " ");
+      Write (mArrayQueue[i]);
       WriteLine ();
    }
 
+   /// <summary>Resizes the array</summary>
+   void ArrayResize () {
+      T[] newArray = new T[Capacity];
+      int j = 0;
+      do {
+         newArray[j] = mArrayQueue[mFront];
+         mFront = (mFront + 1) % Capacity;
+         j++;
+      } while (mFront != (mRear + 1) % Capacity);
+      mArrayQueue = newArray;
+      mFront = 0;
+      mRear = Capacity - 1;
+   }
+   #endregion
+
+   #region Private Data ------------------------------------------
    // Private variable for initialising array and count
    T[] mArrayQueue;
-   int mCount;
+   int mCount, mFront, mRear;
+   #endregion
 }
-
+#endregion
