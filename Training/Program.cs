1d651f4e6e1d486fb2141e48b2363009e6f7686e
Ajay Karthikeyan N <ajaykarthikeyan.n@metamation.com>
2023-12-12 13:04:12 +0530

    A08 - Unary Operator

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,34 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+﻿// ----------------------------------------------------------------------------------------
+// Training 
+// Copyright (c) Metamation India.
+// ----------------------------------------------------------------------------------------
+// Program.cs
+// Unary operator
+// Implement unary operator in expression evaluator
+// ----------------------------------------------------------------------------------------
+namespace Eval;
+using static System.Console;
+
+#region Program ----------------------------------------------------------------------------------------
+internal class Program {
+   private static void Main (string[] args) {
+      Evaluator eval = new ();
+      // Infinite loop for evaluating expressions
+      for (; ; ) {
+         Write (">");
+         string input = ReadLine () ?? "".Trim ();
+         if (input == "") break;
+         // Try catch statements for printing the result and exceptions
+         try {
+            double result = eval.Evaluate (input);
+            ForegroundColor = ConsoleColor.DarkGreen;
+            WriteLine (Math.Round (result, 10));
+         } catch (Exception e) {
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine (e.Message);
+         }
+         ResetColor ();
       }
    }
 }
+#endregion
