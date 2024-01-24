c3ab2fa25c494434de690f27173941d027c15146
Ajay Karthikeyan N <ajaykarthikeyan.n@metamation.com>
2024-01-24 12:58:31 +0530

    Complex NUmbers Test

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+﻿// Complex Numbers
+class ComplexNumbers {
+   public ComplexNumbers (double real, double imaginary) {
+      mReal = real;
+      mImaginary = imaginary;
    }
+   public string GetNum => "(" + mReal.ToString () + ")+(" + mImaginary.ToString () + "i)";
+   public double Norm =>
+           Math.Sqrt (mReal * mReal + mImaginary * mImaginary);
+   public (double, double) Add (ComplexNumbers a) =>
+                      (a.mReal + this.mReal, a.mImaginary + this.mImaginary);
+
+   public (double, double) Sub (ComplexNumbers a, ComplexNumbers b) =>
+                      (a.mReal - b.mReal, a.mImaginary - b.mImaginary);
+
+   double mReal;
+   double mImaginary;
 }
