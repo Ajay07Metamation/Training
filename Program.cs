9c7f1f8243c2fc4dd673bee2374e0afe80c4563d
Ajay Karthikeyan N <ajaykarthikeyan.n@metamation.com>
2024-02-02 15:34:31 +0530

    A14.1 Anagram

--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
-﻿namespace Training {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
-      }
+﻿// ----------------------------------------------------------------------------------------
+// Training 
+// Copyright (c) Metamation India.
+// ----------------------------------------------------------------------------------------
+// Program.cs
+// Anagram
+// To find anagrams from the given text file
+// ----------------------------------------------------------------------------------------
+
+using static System.Console;
+
+internal class Program {
+   private static void Main (string[] args) {
+      // Sort each word in alphabetical order and group them
+      var anagram = File.ReadAllLines ("C:/etc/words.txt")
+          .GroupBy (word => string.Concat (word.OrderBy (ch => ch)))
+          .Where (word => word.Count () > 1) 
+          .OrderByDescending (word => word.Count ()) 
+          .ToList ();
+      anagram.ForEach (g => WriteLine ($"{g.Count ()} {string.Join (", ", g)}"));
+      // Writing the output to a file
+      using (var stream = new StreamWriter ("C:/etc/Anagrams.txt"))
+         anagram.ForEach (word => stream.WriteLine ($"{word.Count ()} {string.Join (", ", word)}"));
    }
 }
